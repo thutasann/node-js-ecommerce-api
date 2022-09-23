d614fa6b1e02efacb450132f88d52efc13944545
thuta_yoma <thutasann@yomabank.com>
2022-09-23 21:41:04 +0630

    JWT applied

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require('jsonwebtoken');
 
 // REGISTER
 router.post("/register", async (req,res) => {
@@ -38,9 +39,22 @@ router.post("/login", async (req,res) => {
         originalPassword !== req.body.password &&
             res.status(401).json("Wrong Credentials");
 
+        
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin
+        }, 
+        process.env.JWT_SEC,
+        { expiresIn: '3d' }
+        )
+
         const { password, ...others} = user._doc;
 
-        res.status(200).json(others);
+        res.status(200).json({
+            ...others, 
+            accessToken,
+            message: "Login success"
+        });
 
     } catch (error) {
         res.status(500).json(error);
